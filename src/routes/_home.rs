1a6d2139b99f9f41cd379757cd8d55ad6ff4c214
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2021-09-09 01:45:56 -0400

    Some controls

---
+++ src/routes/_home.rs
@@ -0,0 +1,84 @@
+use std::{
+	error::Error,
+	fmt::{self, Debug, Display, Formatter}
+};
+use log::debug;
+use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen_futures::JsFuture;
+use yew::{Component, ComponentLink, Html, ShouldRender, html, web_sys::{FileSystemHandle, FileSystemDirectoryHandle, window}};
+use yewtil::future::LinkFuture;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct JsFutureError {
+	err: JsValue,
+}
+
+impl Display for JsFutureError {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+			Debug::fmt(&self.err, f)
+	}
+}
+
+impl Error for JsFutureError {}
+
+impl From<JsValue> for JsFutureError {
+	fn from(value: JsValue) -> Self {
+			Self { err: value }
+	}
+}
+
+async fn pick_directory() -> Result<Option<FileSystemDirectoryHandle>, JsFutureError> {
+	if let Some(w) = window() {
+		let d = JsFuture::from(w.show_directory_picker()).await?;
+		let directory: FileSystemDirectoryHandle = d.dyn_into().unwrap();
+		return Ok(Some(directory));
+	}
+	Ok(None)
+}
+
+pub enum HomeMsg {
+	ShowDirectoryPicker,
+	IndexDirectory,
+}
+
+pub struct Home {
+	link: ComponentLink<Self>,
+}
+
+impl Component for Home {
+	type Message = HomeMsg;
+	type Properties = ();
+
+	fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+		Home {
+			link
+		}
+	}
+
+	fn change(&mut self, _: Self::Properties) -> ShouldRender {
+		false
+	}
+
+	fn update(&mut self, msg: Self::Message) -> ShouldRender {
+		match msg {
+			HomeMsg::ShowDirectoryPicker => {
+				self.link.send_future(async {
+					if let Ok(Some(d)) = pick_directory().await {
+						debug!("{:?}", d);
+					}
+					HomeMsg::IndexDirectory
+				});
+			}
+			HomeMsg::IndexDirectory => {}
+		}
+		false
+	}
+
+	fn view(&self) -> Html {
+		html! {
+			<div>
+				<button onclick=self.link.callback(|_| HomeMsg::ShowDirectoryPicker)>{ "Choose a directory..." }</button>
+			</div>
+		}
+	}
+}
--- src/routes/home.rs
+++ src/routes/home.rs
@@ -1,47 +1,15 @@
-use std::{
-	error::Error,
-	fmt::{self, Debug, Display, Formatter}
-};
 use log::debug;
-use wasm_bindgen::{JsCast, JsValue};
-use wasm_bindgen_futures::JsFuture;
-use yew::{Component, ComponentLink, Html, ShouldRender, html, web_sys::{FileSystemDirectoryHandle, window}};
-use yewtil::future::LinkFuture;
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct JsFutureError {
-	err: JsValue,
-}
-
-impl Display for JsFutureError {
-	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-			Debug::fmt(&self.err, f)
-	}
-}
-
-impl Error for JsFutureError {}
-
-impl From<JsValue> for JsFutureError {
-	fn from(value: JsValue) -> Self {
-			Self { err: value }
-	}
-}
-
-async fn pick_directory() -> Result<String, JsFutureError> {
-	if let Some(w) = window() {
-		let d = JsFuture::from(w.show_directory_picker()).await?;
-		let directory: FileSystemDirectoryHandle = d.dyn_into().unwrap();
-		return Ok(directory.name());
-	}
-	Ok(String::from(""))
-}
+use yew::{ChangeData, Component, ComponentLink, Html, ShouldRender, html, web_sys::{File, Url}};
 
 pub enum HomeMsg {
-	ShowDirectoryPicker,
-	IndexDirectory,
+	IndexDirectory(Vec<File>),
+	NextFile,
+	PreviousFile,
 }
 
 pub struct Home {
+	files: Option<Vec<File>>,
+	index: isize,
 	link: ComponentLink<Self>,
 }
 
@@ -51,6 +19,8 @@ impl Component for Home {
 
 	fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
 		Home {
+			files: None,
+			index: 0,
 			link
 		}
 	}
@@ -61,24 +31,90 @@ impl Component for Home {
 
 	fn update(&mut self, msg: Self::Message) -> ShouldRender {
 		match msg {
-			HomeMsg::ShowDirectoryPicker => {
-				self.link.send_future(async {
-					if let Ok(d) = pick_directory().await {
-						debug!("{}", d);
-					}
-					HomeMsg::IndexDirectory
-				});
+			HomeMsg::IndexDirectory(files) => {
+				self.files = Some(files);
+				true
 			}
-			HomeMsg::IndexDirectory => {}
+			HomeMsg::NextFile => {
+				if let Some(files) = &self.files {
+					self.index = (self.index + 1).rem_euclid(files.len() as isize);
+					return true;
+				}
+				false
+			}
+			HomeMsg::PreviousFile => {
+				if let Some(files) = &self.files {
+					self.index = (self.index - 1).rem_euclid(files.len() as isize);
+					return true;
+				}
+				false
+			}
 		}
-		false
 	}
 
 	fn view(&self) -> Html {
-		html! {
-			<div>
-				<button onclick=self.link.callback(|_| HomeMsg::ShowDirectoryPicker)>{ "Choose a directory..." }</button>
-			</div>
+		match &self.files {
+			Some(files) => {
+				let file = &files[(self.index as usize).rem_euclid(files.len())];
+				let src_url = Url::create_object_url_with_blob(&file.slice().ok().unwrap()).ok();
+				let nav_buttons_class = "text-white bg-gray-700 text-opacity-0 bg-opacity-0 hover:text-opacity-100 hover:bg-opacity-70 transition duration-500 absolute inset-y-0 w-1/12 text-9xl flex place-content-center place-items-center cursor-pointer select-none";
+				html! {
+					// TODO: Figure out how to get content to stretch to fill if too small.
+					<div class="bg-black">
+						{
+							if file.type_().starts_with("audio/") {
+								html! {
+									<audio
+										autoplay=""
+										class="max-h-screen max-w-screen mx-auto object-center"
+										controls=true
+										src={ src_url }
+									/>
+								}
+							} else if file.type_().starts_with("image/") {
+								html! {
+									<img class="max-h-screen max-w-screen mx-auto object-center" src={ src_url } />
+								}
+							} else if file.type_().starts_with("video/") {
+								html! {
+									<video
+										autoplay=""
+										class="max-h-screen max-w-screen mx-auto object-center"
+										controls=true
+										src={ src_url }
+									/>
+								}
+							} else {
+								self.link.send_message(HomeMsg::NextFile);
+								html!()
+							}
+						}
+						<div class=format!("{} {}", nav_buttons_class, "left-0") onclick=self.link.callback(|_| HomeMsg::PreviousFile)>
+							<p>{ "←" }</p>
+						</div>
+						<div class=format!("{} {}", nav_buttons_class, "right-0") onclick=self.link.callback(|_| HomeMsg::NextFile)>
+							<p>{ "→" }</p>
+						</div>
+					</div>
+				}
+			},
+			None => html! {
+				<div>
+					<input type="file" webkitdirectory="" onchange=self.link.callback(move |value| {
+						let mut result = Vec::new();
+						if let ChangeData::Files(files) = value {
+								let files = js_sys::try_iter(&files)
+										.unwrap()
+										.unwrap()
+										.map(|v| File::from(v.unwrap()))
+										.filter(|f| !f.name().starts_with('.'))
+										.filter(|f| f.type_().starts_with("audio/") || f.type_().starts_with("image/") || f.type_().starts_with("video/"));
+								result.extend(files);
+						}
+						HomeMsg::IndexDirectory(result)
+					}) />
+				</div>
+			},
 		}
 	}
 }
